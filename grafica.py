eca8a26398e22f26ce8b4edbf2b4e281a718d67b
ggorotiza1 <gabugoro17@gmail.com>
2022-12-30 17:18:10 -0500

    PrimeraVersion

---
+++ grafica.py
@@ -0,0 +1,12 @@
+from tkinter import messagebox
+
+class uwu():
+    def diosmio ():
+        x= 10
+        y=1
+        ola=x+y
+        return "Camily mi primer pupilo en Python"
+
+
+if __name__ == "__main__": 
+    messagebox.showinfo("Mensaje", uwu.diosmio())
