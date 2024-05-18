2184c1ad8e5cfff80f9efdd2e8baf9b5053697e5
jgarzon97 <jorgegarzonizquierdo@gmail.com>
2024-05-18 15:56:23 -0500

    Corregido el error con la gráfica

--- mCalculation.py
+++ mCalculation.py
@@ -12,13 +12,19 @@ from matplotlib import pyplot
 
 # Realizado por Gabriel Gorotiza
 
-
-def métodoDeBisección(formula: Entry, txt2: Entry, txt3: Entry, trv: ttk.Treeview, plt: pyplot, frame: Frame):
+def métodoDeBisección(
+    formula: Entry,
+    txt2: Entry,
+    txt3: Entry,
+    trv: ttk.Treeview,
+    plt: pyplot,
+    frame: Frame,
+):
     global lbl_resultado
     global xr
     if formula.get() != None and txt2.get() != None and txt3.get() != None:
         formula = formula.get()
-        x = symbols('x')
+        x = symbols("x")
         fn = sympify(formula)
         f = lambdify(x, fn, "numpy")
         crit = 0.0000001
@@ -32,34 +38,42 @@ def métodoDeBisección(formula: Entry, txt2: Entry, txt3: Entry, trv: ttk.Treev
                 messagebox.showerror("Error", "¡Error! Fuera de Rango")
             else:
                 while ea > crit:
-                    xr = (a+b)/2
-                    ea = abs((xr-xr_anterior)/xr)
+                    xr = (a + b) / 2
+                    ea = abs((xr - xr_anterior) / xr)
                     if (f(xr) * f(a)) < 0:
                         b = xr
                     else:
                         a = xr
                     xr_anterior = xr
-                    trv.insert("", END, values=(
-                        i, a, b, xr_anterior, (ea*100)))
+                    trv.insert("", END, values=(i, a, b, xr_anterior, (ea * 100)))
                     i = i + 1
                 plt.scatter(xr, 0, c="red")
                 # plt.annotate(xr_anterior, xy=(xr_anterior, 3.5))
                 lbl_resultado = customtkinter.CTkLabel(
-                    master=frame, text="Raíz encontrada en: " + str(xr_anterior), font=("Roboto", 12))
+                    master=frame,
+                    text="Raíz encontrada en: " + str(xr_anterior),
+                    font=("Roboto", 12),
+                )
                 lbl_resultado.place(x=220, y=640)
 
         except ValueError:
-            messagebox.showwarning(
-                "Atención", "Los intervalos deben ser números")
+            messagebox.showwarning("Atención", "Los intervalos deben ser números")
     else:
         messagebox.showinfo("Atención", "Debe llenar todos los campos")
 
 
-def métodoDeFalsaPosicion(formula: Entry, txt2: Entry, txt3: Entry, trv: ttk.Treeview, plt: pyplot, frame: Frame):
+def métodoDeFalsaPosicion(
+    formula: Entry,
+    txt2: Entry,
+    txt3: Entry,
+    trv: ttk.Treeview,
+    plt: pyplot,
+    frame: Frame,
+):
     global lbl_resultado
     if formula != None and txt2 != None and txt3 != None:
         formula = formula.get()
-        x = symbols('x')
+        x = symbols("x")
         fn = sympify(formula)
         f = lambdify(x, fn, "numpy")
 
@@ -68,9 +82,9 @@ def métodoDeFalsaPosicion(formula: Entry, txt2: Entry, txt3: Entry, trv: ttk.Tr
         crit = 0.0000001
         i = -1
         xr_anterior = 0
-        xr = (a*f(b) - b*f(a)) / (f(b) - f(a))
+        xr = (a * f(b) - b * f(a)) / (f(b) - f(a))
 
-        ea = abs(xr-xr_anterior)
+        ea = abs(xr - xr_anterior)
         try:
             a = float(txt2.get())
             b = float(txt3.get())
@@ -78,26 +92,28 @@ def métodoDeFalsaPosicion(formula: Entry, txt2: Entry, txt3: Entry, trv: ttk.Tr
                 messagebox.showerror("Error", "¡Error! Fuera de Rango")
             else:
                 while ea > crit:
-                    xr_anterior = (a*f(b) - b*f(a)) / (f(b) - f(a))
+                    xr_anterior = (a * f(b) - b * f(a)) / (f(b) - f(a))
                     if f(xr_anterior) * f(a) < 0:
                         ea = abs(xr_anterior - b)
                         b = xr_anterior
-                        i = i+1
+                        i = i + 1
                     elif f(xr_anterior) * f(b) < 0:
                         ea = abs(xr_anterior - a)
                         a = xr_anterior
-                        i = i+1
+                        i = i + 1
                     else:
                         messagebox.showinfo("Error", "Error")
                     trv.insert("", END, values=(i, a, b, xr_anterior, ea))
                 plt.scatter(xr_anterior, 0, c="red")
                 # plt.annotate(xr_anterior, xy=(xr_anterior, 3.5))
                 lbl_resultado = customtkinter.CTkLabel(
-                    master=frame, text="Raíz encontrada en: " + str(xr_anterior), font=("Roboto", 12))
+                    master=frame,
+                    text="Raíz encontrada en: " + str(xr_anterior),
+                    font=("Roboto", 12),
+                )
                 lbl_resultado.place(x=220, y=640)
         except ValueError:
-            messagebox.showwarning(
-                "Atención", "Los intervalos deben ser números")
+            messagebox.showwarning("Atención", "Los intervalos deben ser números")
     else:
         messagebox.showinfo("Atención", "Debe llenar todos los campos")
 
